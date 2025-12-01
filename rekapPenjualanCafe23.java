bcb2d9bb434f99a54ad197a643820f751021e0e0
Silmi <129129210+SilmiCamilia@users.noreply.github.com>
2025-12-01 19:40:27 +0700

    updatezpush

--- rekapPenjualanCafe23.java
+++ rekapPenjualanCafe23.java
@@ -1,74 +1,66 @@
 import java.util.Scanner;
 
 public class rekapPenjualanCafe23 {
-    static Scanner halomok = new Scanner(System.in);
-    static String[] menu = {"kopi", "teh", "susu", "coklat", "latte"};
-    static int[][] penjualan = new int[5][7]; 
+  public static void main(String[] args) {
+        Scanner halomok = new Scanner(System.in);
+        System.out.print("masukkan jumlah menu = ");
+        int jumlahmenuh = halomok.nextInt();
+        halomok.nextLine();
 
-    public static void ingputz() {
-        System.out.println(">>> input penjualan 7 hari <<<");
-        for (int i = 0; i < menu.length; i++) {
+        String[] menu = new String[jumlahmenuh];
+
+        System.out.println("\nmasukkan nama menu:");
+        for (int i = 0; i < jumlahmenuh; i++) {
+            System.out.print("menu ke-" + (i + 1) + " = ");
+            menu[i] = halomok.nextLine();
+        }
+
+        System.out.print("\nmasukkan jumlah hari penjualan = ");
+        int jumlahdayz = halomok.nextInt();
+
+        int[][] penjualan = new int[jumlahmenuh][jumlahdayz];
+
+        System.out.println("\n>>> input penjualan <<<");
+        for (int i = 0; i < jumlahmenuh; i++) {
             System.out.println("\nmenu: " + menu[i]);
-            for (int h = 0; h < 7; h++) {
-                System.out.print("hari " + (h + 1) + " = ");
+            for (int h = 0; h < jumlahdayz; h++) {
+                System.out.print("hari " + (h + 1) + ": ");
                 penjualan[i][h] = halomok.nextInt();
             }
         }
-    }
 
-    public static void nampil() {
-        System.out.println("\n>>> tabel rekap penjualan <<<");
-        System.out.print("menu/hari\t");
-        for (int h = 1; h <= 7; h++) {
-            System.out.print("h" + h + "\t");
-        }
+        System.out.println("\n>>> rekap Ppnjualan <<<");
+        System.out.print("menu\t");
+        for (int h = 1; h <= jumlahdayz; h++) System.out.print("H" + h + "\t");
         System.out.println();
 
-        for (int i = 0; i < menu.length; i++) {
+        for (int i = 0; i < jumlahmenuh; i++) {
             System.out.print(menu[i] + "\t");
-            for (int h = 0; h < 7; h++) {
+            for (int h = 0; h < jumlahdayz; h++) {
                 System.out.print(penjualan[i][h] + "\t");
             }
             System.out.println();
         }
-    }
 
-    public static void menuTertinggi() {
         int maxTotal = -1;
         String menuMax = "";
-
-        for (int i = 0; i < menu.length; i++) {
+        for (int i = 0; i < jumlahmenuh; i++) {
             int total = 0;
-            for (int h = 0; h < 7; h++) {
-                total += penjualan[i][h];
-            }
+            for (int h = 0; h < jumlahdayz; h++) total += penjualan[i][h];
+
             if (total > maxTotal) {
                 maxTotal = total;
                 menuMax = menu[i];
             }
         }
+        System.out.println("\nmenu penjualan tertinggi: " + menuMax + " (" + maxTotal + ")");
 
-        System.out.println("\nmenu dengan total penjualan tertinggi:");
-        System.out.println(menuMax + " : " + maxTotal + " porsi");
-    }
-
-    public static void rata() {
-        System.out.println("\n>>> Rata-rata Penjualan <<<");
-        for (int i = 0; i < menu.length; i++) {
+        System.out.println("\n>>> rata rata penjualan <<<");
+        for (int i = 0; i < jumlahmenuh; i++) {
             int total = 0;
-            for (int h = 0; h < 7; h++) {
-                total += penjualan[i][h];
-            }
-            double avg = total / 7.0;
+            for (int h = 0; h < jumlahdayz; h++) total += penjualan[i][h];
+            double avg = total / (double) jumlahdayz;
             System.out.println(menu[i] + " : " + avg);
         }
     }
-
-    public static void main(String[] args) {
-        ingputz();
-        nampil();
-        menuTertinggi();
-        rata();
-    }
-
 }
