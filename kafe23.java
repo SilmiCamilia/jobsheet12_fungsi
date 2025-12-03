3f0a55dd687af2f16935b299c4c784e7ac01368c
Silmi <129129210+SilmiCamilia@users.noreply.github.com>
2025-12-03 08:08:41 +0700

    upgrade2

--- kafe23.java
+++ kafe23.java
@@ -1,17 +1,12 @@
+import java.util.Scanner;
+
 public class kafe23 {
-     public static void menu(String namaPelanggan, boolean isMember, String kodePromo) {
+     public static void menu(String namaPelanggan, boolean isMember) {
 
         System.out.println("Selamat Datang," + " " + namaPelanggan + "!");
         if (isMember) {
             System.out.println("Anda mendapatkan diskon 10% sebagai member kafe kami.");
         }
-        if (kodePromo.equals("DISKON50")) {
-        System.out.println("Berikan diskon 50%");
-    } else if (kodePromo.equals("DISKON30")) {
-        System.out.println("Berikan diskon 30%");
-    } else {
-        System.out.println("Kode invalid");
-    }
 
         System.out.println("=== Menu Resto Kafe ===");
         System.out.println("1. Kopi Hitam- Rp 15.000");
@@ -23,7 +18,37 @@ public class kafe23 {
         System.out.println("==========================");
         System.out.println("Silahkan pilih menu yang anda inginkan:");
     }
-    public static void main(String[] args) {
-        menu("Andi", true, "DISKON50");
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem, String kodePromo) {
+        int [] hargaItem = {15000, 20000, 22000, 12000, 10000, 18000};
+        int hargaTotal = hargaItem[pilihanMenu -1] * banyakItem;
+        if (kodePromo != null && kodePromo.equals("DISKON50")) {
+            System.out.println("Diskon 50% berhasil!");
+            hargaTotal = hargaTotal / 2; // Diskon 50%
+        } else if (kodePromo != null && kodePromo.equals("DISKON30")) {
+            System.out.println("Diskon 30% berhasil!");
+            hargaTotal = hargaTotal - (int)(hargaTotal * 0.3); 
+        } else if (kodePromo != null && !kodePromo.equals("")) {
+            System.out.println("Kode invalid");
+        }
+        return hargaTotal;
     }
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        return hitungTotalHarga(pilihanMenu, banyakItem, "");
+    }
+    public static void main(String[] args) {
+        Scanner ingputz = new Scanner(System.in);
+
+        menu("Andi", true);
+        System.out.print("Masukkan nomor menu yang anda pesan:");
+        int pilihanMenu = ingputz.nextInt();
+        System.out.print("Masukkan banyak item yang anda pesan:");
+        int banyakItem = ingputz.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total yang harus anda bayar: Rp" + totalHarga);
+
+    }
+
 }
